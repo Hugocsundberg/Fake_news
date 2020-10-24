a0cfd4454b83879dbb8b169c1ab7375468d3d6c5
Hugo Sundberg <hugocsundberg@gmail.com>
2020-10-24 23:49:09 +0200

    created function to get author name by id

--- components/body.php
+++ components/body.php
@@ -1,177 +1,33 @@
 <div class="gridContainer">
+    // print_r($row->headline);
 
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
+    <?php foreach ($data as $row) : ?>
+        <div class="card">
+            <div class="card_top-section w-100">
+                <div class="card_top-text h-100">
+                    <p><?= $row->headline ?></p>
+                </div>
+                <div class="card_top-icons h-100">
+                    <p><?= $row->likes ?></p>
+                    <div class="icon1"></div>
+                    <div class="icon2"></div>
+                </div>
             </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
+            <div class="card_image"></div>
+            <div class="card_main-content">
+                <p>
+                    <?= $row->content ?>
+                </p>
             </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
+            <div class="card_text-fade-away w-100">
+                <div class="card_expand-icon"></div>
             </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
+            <div class="card_bottom-section w-100">
+                <div class="card_author"><?= $row->author ?></div>
+                <div class="card_date"><?php echo gmdate('Y-m-d', $row->date) ?>
+                </div>
             </div>
         </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
-            </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
-            </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
-            </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
-            </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
-            </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
-            </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
-            </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
-            </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card_top-section w-100">
-            <div class="card_top-text h-100">
-                <p>Lopem Ipsum</p>
-            </div>
-            <div class="card_top-icons h-100">
-                <p>3</p>
-                <div class="icon1"></div>
-                <div class="icon2"></div>
-            </div>
-        </div>
-        <div class="card_image"></div>
-        <div class="card_main-content">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris convallis turpis, id placerat tortor dolor non erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra vehicula congue. Praesent eu nisl vel nulla sodales venenatis quis at nisl. Sed iaculis odio maximus magna ornare vehicula. Suspendisse posuere, odio sed blandit faucibus, ante mauris conplacerat tortor dolor non erat.Lorem ipsum dolor sit amet, consecte
-            </p>
-        </div>
-        <div class="card_text-fade-away w-100">
-            <div class="card_expand-icon"></div>
-        </div>
-        <div class="card_bottom-section w-100">
-            <div class="card_author">Author</div>
-            <div class="card_date">2020-10-3</div>
-        </div>
-    </div>
+
+
+    <?php endforeach ?> ?>
--- server-connection.php
+++ server-connection.php
@@ -1,2 +1,13 @@
 <?php
 $data = json_decode(file_get_contents('https://fake-news-backend-1337.herokuapp.com/export.php'));
+
+$getAuthor = function (int $id): string {
+    $author_data = json_decode(file_get_contents('https://fake-news-backend-1337.herokuapp.com/export_author.php'));
+    foreach ($author_data as $author) {
+        if ($author->id === $id) {
+            return $author->author_name;
+        }
+    }
+};
+
+echo $getAuthor(5);
